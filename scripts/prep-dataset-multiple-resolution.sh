cbed680e3e52ccdee9aa1ebf45970340ff0658be
ckrapu@gmail.com <ckrapu@gmail.com>
2025-08-26 09:46:29 -0700

    Update block size

---
+++ scripts/prep-dataset-multiple-resolution.sh
@@ -0,0 +1,20 @@
+IMAGE_SIZE=128
+N_SAMPLES=1000000
+CLEANUP=true
+
+for DOWNSAMPLE_RATIO in 2 4 8 16 32; do
+    echo "Processing downsample ratio: $DOWNSAMPLE_RATIO"
+    papermill prep-dataset.ipynb prep-dataset-$DOWNSAMPLE_RATIO.ipynb \
+     -p image_size $IMAGE_SIZE \
+     -p downsample_ratio $DOWNSAMPLE_RATIO \
+     -p n_samples $N_SAMPLES
+
+    if [ "$CLEANUP" = true ]; then
+        rm -f prep-dataset-$DOWNSAMPLE_RATIO.ipynb
+    fi
+done
+
+echo "Combining datasets..."
+python combine-datasets.py --image-size $IMAGE_SIZE \
+    --ratios 2 4 8 16 32\
+    --output ../data/combined_multi_res.npz
