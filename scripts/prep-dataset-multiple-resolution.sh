fe118a2b5bcb5527e60e3dc7dd0e430a6164869b
ckrapu@gmail.com <ckrapu@gmail.com>
2025-08-27 10:39:29 -0700

    Control grid size in training script

--- scripts/prep-dataset-multiple-resolution.sh
+++ scripts/prep-dataset-multiple-resolution.sh
@@ -1,13 +1,16 @@
 IMAGE_SIZE=128
-N_SAMPLES=1000000
+N_SAMPLES_MAX=3000000
 CLEANUP=true
+N_GRID_UNIT=50
 
 for DOWNSAMPLE_RATIO in 2 4 8 16 32; do
     echo "Processing downsample ratio: $DOWNSAMPLE_RATIO"
     papermill prep-dataset.ipynb prep-dataset-$DOWNSAMPLE_RATIO.ipynb \
      -p image_size $IMAGE_SIZE \
      -p downsample_ratio $DOWNSAMPLE_RATIO \
-     -p n_samples $N_SAMPLES
+     -p n_sample_maxs $N_SAMPLES_MAX \
+     -p n_grid_unit $N_GRID_UNIT \
+     --log-output
 
     if [ "$CLEANUP" = true ]; then
         rm -f prep-dataset-$DOWNSAMPLE_RATIO.ipynb
